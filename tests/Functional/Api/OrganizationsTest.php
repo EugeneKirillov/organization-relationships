f22f3173e0a99b38e21ec6717754a6f7fef59ed2
Eugene Kirillov <kirill0ff@yandex.ru>
2016-01-17 13:47:43 +0300

    OWR-5 Create endpoint 3: Add functional tests for endpoint stub #5

---
+++ tests/Functional/Api/OrganizationsTest.php
@@ -0,0 +1,48 @@
+<?php
+namespace Owr\Tests\Functional\Api;
+
+use Owr\Tests\Functional\WebTestCase;
+
+class OrganizationsTest extends WebTestCase
+{
+    public function testGetRelations()
+    {
+        $params = [
+            'page'  => 1,
+            'count' => 2,
+        ];
+        $this->client->request('GET', '/api/v1/organizations/relations/Jungle Tree', $params);
+
+        $expected = array_slice([
+            [
+                "relationship_type"=> "self",
+                "org_name" => "Jungle Tree",
+            ],
+            [
+                "relationship_type"=> "parent",
+                "org_name" => "Banana tree",
+            ],
+            [
+                "relationship_type" => "parent",
+                "org_name" => "Big banana tree",
+            ],
+            [
+                "relationship_type" => "sister",
+                "org_name" => "Brown Banana",
+            ],
+            [
+                "relationship_type" => "sister",
+                "org_name" => "Green Banana",
+            ],
+            [
+                "relationship_type" => "daughter",
+                "org_name" => "Phoneutria Spider",
+            ],
+            [
+                "relationship_type" => "sister",
+                "org_name" => "Yellow Banana",
+            ],
+        ], ($params['page'] - 1) * $params['count'], $params['count']);
+        $this->assertJsonResponse($expected, 200);
+    }
+}
