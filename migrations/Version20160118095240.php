eb6651649c32293f21b500324134936b8ded7a9e
Eugene Kirillov <kirill0ff@yandex.ru>
2016-01-18 15:30:01 +0300

    OWR-2 Prepare DB structure: migration to prepare db structure #2

---
+++ migrations/Version20160118095240.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Owr\Migration;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Migration to init db structure
+ */
+class Version20160118095240 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $organizations = <<<SQL
+CREATE TABLE `organizations` (
+  `id` INTEGER UNSIGNED,
+  `name` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+SQL;
+        $relations = <<<SQL
+CREATE TABLE `relations` (
+  `source` INTEGER UNSIGNED NOT NULL,
+  `target` INTEGER UNSIGNED NOT NULL,
+  `type` ENUM("parent", "sibling", "child") CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`source`, `target`),
+  KEY `source` (`source`),
+  KEY `target` (`target`),
+  FOREIGN KEY (`source`) REFERENCES `organizations` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (`target`) REFERENCES `organizations` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB
+SQL;
+
+        $this->addSql($organizations);
+        $this->addSql($relations);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->addSql('DROP TABLE `relations`');
+        $this->addSql('DROP TABLE `organizations`');
+    }
+}
