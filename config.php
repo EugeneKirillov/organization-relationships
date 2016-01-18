cedc3ad901adcd5dbacd97f9c74f950c4f4545ae
Eugene Kirillov <kirill0ff@yandex.ru>
2016-01-18 15:28:08 +0300

    OWR-2 Prepare DB structure: create cli for running DB migrations #2

--- config.php
+++ config.php
@@ -1,5 +1,12 @@
 <?php
 // service configurations and properties
 return [
-
+    'db' => [
+        'driver'   => 'pdo_mysql',
+        'host'     => '127.0.0.1',
+        'port'     => '3306',
+        'dbname'   => 'organizations',
+        'user'     => 'homestead',
+        'password' => 'secret',
+    ],
 ];
